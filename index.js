f027b6e47867aaed0a98d8602c892b41dfbda609
Dan Hintz <dhintz@live.com>
2019-06-16 23:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    result = "No can do.";
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  city = "NYC" ? "Ok, sounds good." : "No go.";
+}
