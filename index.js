d93a432f8331120a14f0d43ec0a986d1079bf603
Dan Hintz <dhintz@live.com>
2019-06-17 00:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function ternaryCheckCity(city) {
   const result = city === "NYC" ? "Ok, sounds good." : "No go.";
   return result
 }
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break
+    case "not as generous":
+      return "Thank you.";
+      break
+    default:
+      return "Bye.";
+  }
+}
